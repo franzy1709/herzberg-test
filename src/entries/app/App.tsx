5fc23c4a2cd432f7febc613335d32de18f4c1325
franzy1709 <bannkov.evgeny@gmail.com>
2020-12-14 23:24:32 +0300

    [multi-entries] realized

--- src/entries/app/App.tsx
+++ src/entries/app/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Herzberg } from './components/internal/herzberg/herzberg';
+import { Herzberg } from '../../components/internal/herzberg/herzberg';
+// import { Herzberg } from './components/internal/herzberg/herzberg';
 
 function App() {
   return (
---
+++ src/entries/widget/HerzbergWidget.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './HerzbergWidget.css';
+import { Herzberg } from '../../components/internal/herzberg/herzberg';
+
+export function HerzbergWidget() {
+  return (
+    <div className="HerzbergWidget">
+      <Herzberg 
+        testQuestions={[
+          ['Для тебя важна хорошо оплачиваемая работа, даже если ты не получаешь должного признания.',
+          'Для тебя важно признание и похвала, даже если оплата не так высока.']
+        ]}
+      />
+    </div>
+  );
+}
